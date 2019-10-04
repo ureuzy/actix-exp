e7c4c871696c8a98100cf16048da2b4417eaeb61
masanetes <masanetes@gmail.com>
2019-10-04 14:08:45 +0900

    add middleware logger

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ mod controllers;
 mod models;
 mod router;
 
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 
 fn main() -> std::io::Result<()> {
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
-        App::new().configure(router::routes)
+        App::new()
+            .wrap(Logger::default())
+            .configure(router::routes)
     }).bind("0.0.0.0:8080")?.run()
+
 }
